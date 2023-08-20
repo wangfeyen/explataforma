9288ea1a3ef75b33e9ec8b0c54880005adb75068
wangfeyen <wangfeyen@gmail.com>
2023-08-20 19:10:25 -0300

    enunciado

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021. A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
+
+//Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
+
 const prompt = require("prompt-sync")();
 const nome = prompt("Digite seu nome completo: ");
 let ano_valido=false
